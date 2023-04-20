0e4a82e7c80dff20c5ed93932be02c174faf6022
Tomasz Stanisz <fremen1990@gmail.com>
2023-04-21 00:55:35 +0200

    :art: Section 2: File Based Routing and Navigation
    - Adding Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<h1>Hi man!! I work on NextJS 13!!</h1>
+      <h1 className="text-black-500 text-7xl">
+        Hi man!! I work on NextJS 13!!
+      </h1>
     </main>
-  )
+  );
 }
